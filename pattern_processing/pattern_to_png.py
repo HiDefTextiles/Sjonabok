d0a4edf99d99844867163e4afa29e946bff1524a
tungufoss <helgaingim@hi.is>
2024-08-15 19:39:09 +0000

    Pillow uses the color-palette and allows for transparent png

--- pattern_processing/pattern_to_png.py
+++ pattern_processing/pattern_to_png.py
@@ -1,37 +1,40 @@
-# pattern_to_png.py
-
-import cv2
+from PIL import Image
 import numpy as np
 from .utils import read_txt_to_matrix
 
 COLOR_PALETTE = [
-    (16, 9, 159),  # HI-Blue
-    (214, 31, 105),  # HI-Pink
-    (244, 184, 206),  # HI-Pink-Light
-    (45, 210, 192)  # HI-Green
+    (0, 0, 0, 0),  # Transparent (RGBA) for 0
+    (16, 9, 159, 255),  # HI-Blue for 1
+    (214, 31, 105, 255),  # HI-Pink for 2
+    (244, 184, 206, 255),  # HI-Light Pink for 3
+    (45, 210, 192, 255)  # HI-Green for 4
 ]
 
 
 def apply_color_palette(matrix, palette):
     """
-    Applies a color palette to a binary matrix.
+    Applies a color palette with transparency to a binary matrix.
 
     :param matrix: Binary matrix (numpy array)
-    :param palette: List of RGB tuples representing the color palette
-    :return: Colored image matrix
+    :param palette: List of RGBA tuples representing the color palette
+    :return: Colored image matrix with an alpha channel for transparency
     """
     h, w = matrix.shape
-    color_image = np.zeros((h, w, 3), dtype=np.uint8)
+    color_image = np.zeros((h, w, 4), dtype=np.uint8)  # Note the 4 channels (RGBA)
 
-    for i in range(1, len(palette) + 1):
-        color_image[matrix == i] = palette[i - 1]
+    for i in range(len(palette)):
+        color_image[matrix == i] = palette[i]
+
+    # Have a warning if there is a color in the matrix that is not in the palette
+    assert len(np.unique(matrix)) <= len(
+        palette), "There are more colors in the matrix than in the palette."
 
     return color_image
 
 
 def convert_txt_to_png(input_txt, output_png):
     """
-    Converts a TXT file containing a binary matrix to a PNG image using a color palette.
+    Converts a TXT file containing a binary matrix to a PNG image with transparency.
 
     :param input_txt: Path to the input TXT file
     :param output_png: Path to the output PNG file
@@ -42,5 +45,8 @@ def convert_txt_to_png(input_txt, output_png):
     # Apply color palette
     colored_image = apply_color_palette(matrix, COLOR_PALETTE)
 
-    # Save the image as PNG
-    cv2.imwrite(output_png, colored_image)
+    # Convert the image array to a Pillow Image object
+    image = Image.fromarray(colored_image, mode='RGBA')
+
+    # Save the image as PNG with transparency
+    image.save(output_png)
